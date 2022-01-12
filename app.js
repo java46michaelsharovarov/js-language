fb452df479130a99e7159d8aa84e984a7136dd7e
Mik <m.shac86@gmail.com>
2022-01-12 18:01:17 +0200

    cw_1

---
+++ app.js
@@ -0,0 +1,22 @@
+console.log("Hello World!");
+// for(var i=0; i<3; i++){
+//     setTimeout(function(){
+//         console.log(i);
+//     })
+// }
+// for(let i=0; i<3; i++){
+//     setTimeout(function(){
+//         console.log(i);
+//     })
+// }
+// const fun = function(a, b) {
+//     return a + b;
+// }
+console.log("12" + 12);
+console.log("12" - 12);
+console.log("ab" - 12);
+console.log(+"12" + 12);
+console.log("12" + 12, '"12" + 12');
+console.log(`+"12" + 12 = ${+"12" + 12}`);
+
+console.log("a" + "a" * "s" + "as");
