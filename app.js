b7e0e1960f240b5718eedc006614348085653ffb
Mik <m.shac86@gmail.com>
2022-01-14 20:32:29 +0200

    HW_11

--- app.js
+++ app.js
@@ -1,32 +1,29 @@
-let num = 12092;
-let codingString = ".--";
-console.log(`Coding string: "${codingString}"  Encoded number: "${num}" => "${validation(codingString)(num,codingString)}"`);
+let num = 120923;
+let codingString = ".-.";
+console.log(`Coding string: "${codingString}"  Encoded number: "${num}" => "${encode(num,codingString)}"`);
 function encode(num, codingString){
     let res = "";
+    let valid = true;
     const base = codingString.length;
-    do {
-        const digit = Math.trunc(num % base);
-        const symb = getSymbol(digit,codingString);
-        res = symb + res;
-        num = Math.trunc(num / base);
-    } while(num >= 1);
+    for(let i = 0; i < codingString.length; i++){
+        sub = codingString[i];
+        if (codingString.indexOf(sub) != codingString.lastIndexOf(sub)) {
+            res = "CANNOT BE ENCODED!!! The encoding string contains repeated characters!"; 
+            valid = false;  
+            break;       
+        }
+    }  
+    if(valid){
+        res = "";  
+        do {
+            const digit = Math.trunc(num % base);
+            const symb = getSymbol(digit,codingString);
+            res = symb + res;
+            num = Math.trunc(num / base);
+        } while(num >= 1);
+    }   
     return res;
 }
 function getSymbol(digit,codingString){
     return codingString[digit];
-}
-function validation(codingString){
-    let sub;
-    let res = encode;
-    for(let i = 0; i < codingString.length; i++){
-        sub = codingString[i];
-        if (codingString.indexOf(sub) != codingString.lastIndexOf(sub)) {
-            res = logError;   
-            break;       
-        }
-    }
-    return res;
-}
-function logError(){
-    return "CANNOT BE ENCODED!!! The encoding string contains repeated characters!";
 }
