a42fa09f06802ef7a69c94662f906e1bf6c00f40
Mik <m.shac86@gmail.com>
2022-01-14 17:56:55 +0200

    HW_11

--- app.js
+++ app.js
@@ -35,36 +35,38 @@
 // console.log(encode(10,2));
 
 let num = 1256;
-let codingString = ".-";
-console.log(`Encoded number - "${num}" in coding string "${codingString}" is : "${encode(num,codingString)}"`);
+let codingString = "01234567";
+// console.log(`Encoded number - "${num}" in coding string "${codingString}" is : "${encode(num,codingString)}"`);
+console.log(`Encoded number - "${num}" in coding string "${codingString}"${validation(codingString)(num,codingString)}`);
 
 function encode(num, codingString){
     let res = "";
     let base = codingString.length;
     do {
-        let digit = num % base;
-        digit = Math.trunc(digit);
+        let digit = Math.trunc(num % base);
         const symb = getSymbol(digit,codingString);
         res = symb + res;
-        num = num / base;
-        num = Math.trunc(num);
+        num = Math.trunc(num / base);
     } while(num >= 1);
-    return res;
+    return " is : " + `"${res}"`;
 }
 function getSymbol(digit,codingString){
     return codingString[digit];
 }
-// function validation(codingString){
-//     let sub, res;
-//     for(let i = 0; i < codingString.length; i++){
-//         sub = codingString[i];
-//         if (codingString.indexOf(sub) != codingString.lastIndexOf(sub)) {
-//             console.log("ERROR");  
-//             res = false;          
-//         }
-//         res = true;
-//     }
-//     return res;
-//     // console.log(`"${sub}" is ${codingString.indexOf(sub)}; ${codingString.lastIndexOf(sub)}`);
-// }
-// // chek var
+function validation(codingString){
+    let sub;
+    let res = encode;
+    for(let i = 0; i < codingString.length; i++){
+        sub = codingString[i];
+        if (codingString.indexOf(sub) != codingString.lastIndexOf(sub)) {
+            res = logError;   
+            break;       
+        }
+    }
+    return res;
+    // console.log(`"${sub}" is ${codingString.indexOf(sub)}; ${codingString.lastIndexOf(sub)}`);
+}
+function logError(num, codingString){
+    return ", CANNOT BE ENCODED!!! The encoding string contains repeated characters!";
+}
+// chek var
