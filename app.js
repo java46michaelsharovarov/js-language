747c1bff1ec67b45621a832083fa88e02204ea07
Mik <m.shac86@gmail.com>
2022-01-21 20:02:49 +0200

    HW_13

--- app.js
+++ app.js
@@ -1,58 +1,18 @@
-function displayMatrix(matrix) {
-         for (let i = 0; i < matrix.length; i++) {
-                 let row = '';
-                 for (let j = 0; j < matrix[i].length; j++) {
-                    row = spacesInLine(row,matrix[i][j]);
-                 }
-                 console.log(row);
-         }  
+function myForEach(array, callbackFn){
+    for(let i = 0; i < array.length; i++){
+        callbackFn(array[i], i, array);
     }
-function matrixTransp(matrix) {
-         for (let j = 0, i = 0; j < matrix[i].length; j++) {
-                 let row = '';
-                 for (let i = 0; i < matrix.length; i++) {
-                    row = spacesInLine(row,matrix[i][j]);
-                 }
-                 console.log(row);
-         }  
-    }
-function spacesInLine(row, element){
-    if(element > 9 && element <= 99){
-        row = row + element + '   '                         
-     }
-     else if(element > 99 && element <= 999){                      
-        row = row + element + '  '   
-     }
-     else if(element > 999){
-        row = row + element + ' '
-     } 
-     else{
-        row = row + element + '    '
-     }
-    return row;
-}
+};
+function myMap(array, callbackFn) {
+    const arrayRes = [];
+    myForEach(array,function(elt){arrayRes.push(callbackFn(elt))});
+    return arrayRes;
+} 
 
-const matrix = [[1, 22, 3], [4, 1312, 659], [71, 8, 9], [100, 0, 12]];
-displayMatrix(matrix);
-console.log();
-matrixTransp(matrix);
-console.log();
-
-const arHw = [13, 28, 4, 15, 25, -10, 40, 17,27];
-console.log(`arHW is ${arHw.toString()}`);
-arHw.sort(function (a, b){
-   if(a % 2 != 0 && b % 2 != 0){
-      return b - a;
-   }
-   else if(a % 2 == 0 && b % 2 == 0){
-         return a - b;
-   }
-   else if(a % 2 != 0 && b % 2 == 0){
-         return  1;
-   }
-   else if(a % 2 == 0 && b % 2 != 0){
-         return -1;
-   }
-        
-});
-console.log(`arHW sorted is ${arHw.toString()}`);
+const ar = [1, -12, 8, -87, 0, 56];
+let str = '';
+myForEach(ar, n => str += '#' + n);
+console.log(`myForEach: ${str}`);
+myForEach(ar, (n, i, a) => console.log(`${i + 1} of ${a.length} - ${n}`));
+const ar2 = myMap(ar, n => n * 2);
+console.log(ar2);
