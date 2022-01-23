842d327f6d1df6e2b2632948de6243bca6812abc
Mik <m.shac86@gmail.com>
2022-01-23 22:20:15 +0200

    HW_13_testFn

--- app.js
+++ app.js
@@ -5,14 +5,18 @@ function myForEach(array, callbackFn){
 };
 function myMap(array, callbackFn) {
     const arrayRes = [];
-    myForEach(array,function(elt){arrayRes.push(callbackFn(elt))});
+    myForEach(array, (n,i,a) => arrayRes.push(callbackFn(n, i , a)));
     return arrayRes;
 } 
 
-const ar = [1, -12, 8, -87, 0, 56];
-let str = '';
-myForEach(ar, n => str += '#' + n);
-console.log(`myForEach: ${str}`);
-myForEach(ar, (n, i, a) => console.log(`${i + 1} of ${a.length} - ${n}`));
-const ar2 = myMap(ar, n => n * 2);
-console.log(ar2);
+let test = () => {
+    const ar = [1, -12, 8, -87, 0, 56];
+    let str = '';
+    myForEach(ar, n => str += '#' + n);
+    console.log(`myForEach: ${str}`);
+    myForEach(ar, (n, i, a) => console.log(`${i + 1} of ${a.length} - ${n}`));
+    const ar2 = myMap(ar, n => n * 2);
+    console.log(ar2);
+}
+
+test();
