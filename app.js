643f40fa3dcaa3c8cbf76b6d749baffd5a372fc8
Mik <m.shac86@gmail.com>
2022-01-25 17:58:44 +0200

    HW_14_1

--- app.js
+++ app.js
@@ -3,20 +3,34 @@ function myForEach(array, callbackFn){
         callbackFn(array[i], i, array);
     }
 };
-function myMap(array, callbackFn) {
+function myFilter(array, callbackFn) {
     const arrayRes = [];
-    myForEach(array, (n,i,a) => arrayRes.push(callbackFn(n, i , a)));
-    return arrayRes;
-} 
+    myForEach(array, (n, i, a) => callbackFn(n, i, a) ? arrayRes.push(n) : -1);
+    return arrayRes;    
+};
+function myReduce(array, callbackFnReduce, vol) {
+    let res;
+    if(vol == undefined){
+        const array2 = array.slice();
+        res = array2.shift();
+        myForEach(array2, n => res = callbackFnReduce(res, n));
+    }
+    else{    
+    res = vol;
+    myForEach(array, n => res = callbackFnReduce(res, n));
+    }
+    return res;
+};
 
 let test = () => {
-    const ar = [1, -12, 8, -87, 0, 56];
-    let str = '';
-    myForEach(ar, n => str += '#' + n);
-    console.log(`myForEach: ${str}`);
-    myForEach(ar, (n, i, a) => console.log(`${i + 1} of ${a.length} - ${n}`));
-    const ar2 = myMap(ar, n => n * 2);
-    console.log(ar2);
-}
+    const ar = [6, 2, 8, 7, 5, 56];
+    const arEvenOdd = myFilter(ar, (n, _i, a) => a.length % 2 == 0 ? n % 2 == 0 : n % 2 == 1);
+    console.log(arEvenOdd);
+    let res = myReduce(ar, (res, n) => res + n, 1); 
+    console.log(`Reduce with initialValue: ${res}`);
+    res = myReduce(ar, (res, n) => res + n); 
+    console.log(`Reduce without initialValue: ${res}`);
+    
+};
 
 test();
