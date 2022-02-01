46a66fd6b9d6a08c3888f7d3b5e38f3eff0bd675
Mik <m.shac86@gmail.com>
2022-02-01 20:40:47 +0200

    HW_16_add_Task_2

--- app.js
+++ app.js
@@ -15,3 +15,26 @@ d.then(function(res){ console.log('2 ', res); return 'b'; });
 
 d.then(function(res){ console.log('3 ', res); return 'c'; });
 d.resolve('hello');
+
+
+function MyArray(vol) {
+    this.array = [];
+    this.vol = vol;
+}
+MyArray.prototype.get = function (index) {
+    return this.array[index] = this.array[index] || this.vol;
+} 
+MyArray.prototype.set = function (index, vol) {
+    this.array[index] = vol;
+}
+MyArray.prototype.setValue = function (vol) {
+    this.array = [];
+    this.vol = vol;
+}
+const myArray = new MyArray(10);
+console.log(myArray.get(100)) // displayed out 10
+myArray.set(100, 500)         //sets 500 at index 100
+console.log(myArray.get(100)) //displayed out 500
+myArray.setValue(300);
+console.log(myArray.get(100)) //displayed out 300
+console.log(myArray.get(200)) //displayed out 300
