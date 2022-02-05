809c225ab5a264a92e1b8faac14cf2cf123d5e00
Mik <m.shac86@gmail.com>
2022-02-05 10:47:25 +0200

    CW_16

--- app.js
+++ app.js
@@ -1,39 +1,91 @@
-function Deferred() {
-    this.array = [];
+class Person {
+    #id;
+    #name;
+    constructor(id, name) {
+            this.#id = id;
+            this.#name = name
+    }
+    getId() {
+            return this.#id;
+    }
+    getName() {
+            return this.#name
+    }
+    toString() {
+            return `id: ${this.#id}; name: ${this.#name};`
+    }
 }
-Deferred.prototype.then = function (element) {
-    this.array.push(element); 
+const person = new Person(123, 'Moshe');
+console.log(`person is ${person}`)
+class Employee extends Person {
+    #salary;
+    constructor(id, name, salary) {
+            super(id, name); //call the constructor of the class Person
+            this.#salary = salary;
+    }
+    computeSalary() {
+            return this.#salary
+    }
+    toString() {
+            return super.toString() + ` salary: ${this.computeSalary()}`
+    }
 }
-Deferred.prototype.resolve = function (res) {
-    this.array.forEach(n => res = n(res));
+const person2 = new Employee(124, "Sara", 5000);
+console.log(`person2 is ${person2}`)
+console.log(typeof(person2)) // just object
+console.log(person2.constructor.name) // only this way JS allow getting constructor name
+class Child extends Person {
+    #kindergarten
+    constructor(id, name, kindergarten) {
+         super(id, name);
+         this.#kindergarten = kindergarten   
+    }
+    getKindergarten() {
+            return this.#kindergarten;
+    }
+    toString() {
+            return `${super.toString()} kindergarten: ${this.#kindergarten}`
+    }
 }
-const d = new Deferred()
-d.then(function(res){ console.log('1 ', res); return 'a'; });
+const person3 = new Child(125, 'Yakob', 'Shalom');
+console.log(`person3 is ${person3}`)
 
-d.then(function(res){ console.log('2 ', res); return 'b'; });
+class WageEmployee extends Employee {
+    #hours
+    #wage
+    constructor(id, name, salary, hours, wage) {
+            super(id, name, salary)
+            this.#hours = hours;
+            this.#wage = wage;
+    }
+    computeSalary() {
+            return super.computeSalary() + this.#hours * this.#wage
+    }
+    
 
-d.then(function(res){ console.log('3 ', res); return 'c'; });
-d.resolve('hello');
-
-
-function MyArray(vol) {
-    this.array = [];
-    this.vol = vol;
 }
-MyArray.prototype.get = function (index) {
-    return this.array[index] ?? this.vol;
-} 
-MyArray.prototype.set = function (index, vol) {
-    this.array[index] = vol;
+const person4 = new WageEmployee(126, 'Asaf', 1000, 10, 100);
+console.log(`person4 is ${person4}`)
+/**************************************************************HW #17 definition */
+const persons = [
+    new Child(100, 'Olya', 'Shalom'),
+    new Child(101, 'Serega', "Boker"),
+    new Child(102, 'Kolya', 'Shalom'),
+    new Employee(103, 'Vasya', 1000),
+    new WageEmployee(104, 'Tolya', 1000, 10, 100)
+]
+function countOfPersonType(persons, type) {
+    //return count of persons of the given type
+    //Example:
+    //countOfPersonType(persons, 'WageEmployee') ---> 1
 }
-MyArray.prototype.setValue = function (vol) {
-    this.array = [];
-    this.vol = vol;
+function computeSalaryBudget(persons) {
+    //returns total salary of all employee objects in the given array
+    //Example:
+    //computeSalaryBudget(persons) ---> 3000
 }
-const myArray = new MyArray(10);
-console.log(myArray.get(100)) // displayed out 10
-myArray.set(100, 0)         //sets 500 at index 100
-console.log(myArray.get(100)) //displayed out 500
-myArray.setValue(300);
-console.log(myArray.get(100)) //displayed out 300
-console.log(myArray.get(200)) //displayed out 300
+function countChildrenGindergarten(persons, kindergarten) {
+    //returns number of children in the given kindergarten
+    //Example:
+    //countChildrenGindergarten(persons, 'Shalom') ---> 2
+}
