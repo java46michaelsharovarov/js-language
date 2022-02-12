0b9f71d1f20a950be48233fa9c7166499e29442b
Mik <m.shac86@gmail.com>
2022-02-12 13:44:06 +0200

    HW_18

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 function getRandomNumber(min, max) {
         min > max ? [min, max] = [max, min] : 0;
-         //if min > max then you should swap values without additional variable in one line code
+        //if min > max then you should swap values without additional variable in one line code
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 function concatinate(prefix) {
-        return text => prefix + text;   
+        return message => prefix + message;   
 }
         
 console.log(getRandomNumber(0, 1)); //Example getRandomNumber(0, 1) -> returns number that is either 0 or 1
