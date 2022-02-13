b8b85e00f4309b83121a1a25ef682bbe544550a7
Mik <m.shac86@gmail.com>
2022-02-13 20:58:02 +0200

    HW_18_upgraded

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 function getRandomNumber(min, max) {
-        min > max ? [min, max] = [max, min] : 0;
+        min <= max || ([min, max] = [max, min]);
         //if min > max then you should swap values without additional variable in one line code
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+        return Math.floor(Math.random() * (max - min + 1) + min);
 }
-function concatinate(prefix) {
+function concatenate(prefix) {
         return message => prefix + message;   
 }
         
-console.log(getRandomNumber(0, 1)); //Example getRandomNumber(0, 1) -> returns number that is either 0 or 1
-const concatApp = concatinate('App - ');
+console.log(getRandomNumber(1.2, 0.3)); //Example getRandomNumber(0, 1) -> returns number that is either 0 or 1
+const concatApp = concatenate('App - ');
 const concatMessage = concatApp('Test status: Done');
 console.log(concatMessage); //there should be displayed out App - Test status: Done
